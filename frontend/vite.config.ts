88dadabdfd181d635c0c3549fb9ec81cf3b79c27
mravinash222324-dev <mr.avinash222324@gmail.com>
2025-10-16 17:23:53 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+export const BASE_URL = "https://project-management-system-1-2cwr.onrender.com";
 
 // https://vite.dev/config/
 export default defineConfig({
